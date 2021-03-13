2c82c66ff6911d0d52e11c901e0e6f308e304f15
PlakaSorgu <1muharremcandan@gmail.com>
2021-03-13 22:56:42 +0300

    15. Gün Son

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -3,6 +3,9 @@ using Business.BusinessAspect.Autofac;
 using Business.CCS;
 using Business.Constans;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Caching;
+using Core.Aspects.Autofac.Performance;
+using Core.Aspects.Autofac.Transaction;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Business;
@@ -24,26 +27,23 @@ namespace Business.Concrete
     {
         IProductDal _productDal;
         ICategoryService _categoryService;
-
-
         public ProductManager(IProductDal productDal, ICategoryService categoryService)
         {
             _productDal = productDal;
             _categoryService = categoryService;
 
         }
-
-
-        // loglama = yapılan metoddaki işlemleri kadetme yöntemi
+        // loglama = yapılan metoddaki işlemleri kaydetme yöntemi
         [SecuredOperation("product.add,admin")]
-        [ValidationAspect(typeof(ProductValidator))]       
+        [ValidationAspect(typeof(ProductValidator))]
+        [CacheRemoveAspect("IProductService.Get")] // ilkte database den getiriir veriyi ikincide ise eski cache deki veriyi getiriri
         public IResult Add(Product product)
         {
             //business codes  -->iş ihtiyaçlarına uygunluk, meselaehliyet vermek için gerekli ölçütlere sahip mi
             // validation codes --> doğrulama kısmı isim şu kadar olmalı vs vs min max karakter.(Yapısı ile ilgili olnlar bura)
 
             IResult result = BusinessRules.Run(CheckIfProductNameExists(product.ProductName),
-                CheckIfProductCountOfCategoryCorrect(product.CategoryId),CheckIfCategoryNumberMuch());
+                CheckIfProductCountOfCategoryCorrect(product.CategoryId), CheckIfCategoryNumberMuch());
             if (result != null)
             {
                 return result;
@@ -53,6 +53,8 @@ namespace Business.Concrete
 
         }
 
+        [PerformanceAspect(5)]
+        [CacheAspect]
         public IDataResult<List<Product>> GetAll()
         {
             if (DateTime.Now.Hour == 20)
@@ -63,12 +65,12 @@ namespace Business.Concrete
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductsListed);
 
         }
-
         public IDataResult<List<Product>> GetAllByCategoryId(int id)
         {
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.CategoryId == id));
         }
 
+        [CacheAspect]
         public IDataResult<Product> GetById(int productId)
         {
             return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
@@ -84,6 +86,8 @@ namespace Business.Concrete
             return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails());
         }
 
+        [ValidationAspect(typeof(ProductValidator))]
+        [CacheRemoveAspect("IProductService.Get")]
         public IResult Update(Product product)
         {
             if (CheckIfProductCountOfCategoryCorrect(product.CategoryId).Success)
@@ -93,6 +97,7 @@ namespace Business.Concrete
             }
             return new ErrorResult();
         }
+
         private IResult CheckIfProductCountOfCategoryCorrect(int categoryId)
         {
             var result = _productDal.GetAll(p => p.CategoryId == categoryId).Count;
@@ -103,6 +108,7 @@ namespace Business.Concrete
             }
             return new SuccessResult();
         }
+
         private IResult CheckIfProductNameExists(string productName)
         {
             var result = _productDal.GetAll(p => p.ProductName == productName).Any();
@@ -121,6 +127,20 @@ namespace Business.Concrete
                 return new ErrorResult(Messages.CategoryLimitExceded);
             }
             return new SuccessResult();
+        }
+
+        [TransactionScopeAspect]
+        public IResult AddTransactionalTest(Product product)
+        {
+            
+            Add(product);
+            if (product.UnitPrice < 10 )
+            {
+                throw new Exception("");
+            }
+            
+            Add(product);
+            return null;
         }
     }
 }
